59453fd53b57ef51c8b9082aef203651086d8d88
Juan Ignacio Fassio <juanignacio_fassio97@hotmail.com>
2023-03-27 17:59:49 -0300

    Primer commit rama de juani

--- src/e11_ejercicio/Gato.java
+++ src/e11_ejercicio/Gato.java
@@ -2,6 +2,11 @@ package e11_ejercicio;
 
 public class Gato {
     public static void main(String[] args) {
+        
+        int ojos = 2;
+        int patas = 4;
+        int cola = 1;
+        
     }
     
 }
